a9ded3a2b01b0454f1ed1b405b52945977f2bd91
brazrei <79328469+brazrei@users.noreply.github.com>
2022-04-07 14:37:56 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -579,8 +579,9 @@ function plotaMarca(lat, lng, loc) {
         let strInfoICA = spanBold("Fonte:") + " * Quadro 3.2, Página 22 - NSCA 54-5/2020"
         //let strInfoICA = spanBold("Fonte:") + " * Quadro 3.1, Página 21 - ICA 54-3/2007"
         desc = `${desc}<br><br>${strCDA}<h5>${strAlerta}</h5>${strLegenda}${strLegendaDC}<br><br>${strInfoICA}`
-	desc = desc.replace("color:yellow","color:yellow; background-color: #555; padding: 2px;")
-	desc = desc.replace("color:red","color:red; background-color: #aaa; padding: 2px;")
+	desc = desc.replace("color:yellow","color:yellow; background-color: #555; padding: 5px;border-radius: 8px;")
+	desc = desc.replace(/color:black/g,"color:black; background-color: #eee; padding: 5px;border-radius: 8px;")
+	desc = desc.replace(/color:red/g,"color:red; background-color: #ccc; padding: 5px;border-radius: 8px;")
 
         var m = addMarker(L.marker([lat, lng], { icon: icon }), loc, restricao)
 
